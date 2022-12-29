225ade44484ebf9ef531e2891be3e5908bdc0e83
Luã Bida Vacaro <luabidaa@gmail.com>
2022-12-29 10:58:10 -0300

    Fix beat initialization

--- containers/scripts/start_celery_beat.sh
+++ containers/scripts/start_celery_beat.sh
@@ -2,4 +2,4 @@
 
 echo "Starting celery worker"
 
-exec celery --workdir /opt/services/crawlclima/celery --config celeryconfig -A tasks worker -B --loglevel=DEBUG -s /tmp/celerybeat-schedule --pidfile /tmp/celerybeat.pid
+exec celery --workdir /opt/services/crawlclima --config config/celeryconfig -A tasks worker -B --loglevel=DEBUG -s /tmp/celerybeat-schedule --pidfile /tmp/celerybeat.pid
